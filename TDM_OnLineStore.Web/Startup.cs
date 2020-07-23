56550319ca5127e51bd5acdda797c7a0d4bc0ab1
Theo Dias Mitchell <theomitchell@gmail.com>
2020-07-23 12:16:26 -0400

    Revert "Inicial - Models"
    This reverts commit b1d911ab71ea19e10544ec662ed8942f5c3e8031.

--- TDM_OnLineStore.Web/Startup.cs
+++ TDM_OnLineStore.Web/Startup.cs
@@ -51,7 +51,7 @@ namespace TDM_OnLineStore.Web
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller}/{action=Index}/{id?}");         //{id?}  =>  Allows id to be null
+                    template: "{controller}/{action=Index}/{id?}");
             });
 
             app.UseSpa(spa =>
@@ -63,11 +63,7 @@ namespace TDM_OnLineStore.Web
 
                 if (env.IsDevelopment())
                 {
-                    ////RUNNING FROM THE ASP.NET Core SERVER (IIS)
-                    //spa.UseAngularCliServer(npmScript: "start");
-
-                    //RUNNING FROM THE ANGULAR SERVER
-                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200/");
+                    spa.UseAngularCliServer(npmScript: "start");
                 }
             });
         }
