7e924689d6189cd027789083fc42a1c2878265e2
Theo Dias Mitchell <theomitchell@gmail.com>
2020-08-24 11:04:45 -0400

    Revert "Product Component and Login Folder Structure"
    This reverts commit 4c6c7dc9f658df6de20140de58c7cf0b2e0a8633.

--- TDM_OnLineStore.Web/ClientApp/src/app/app.module.ts
+++ TDM_OnLineStore.Web/ClientApp/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
@@ -17,8 +16,7 @@ import { ProductComponent } from './product/product.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent,
-    ProductComponent
+    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -28,7 +26,6 @@ import { ProductComponent } from './product/product.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-{ path: 'product', component: ProductComponent },
     ])
   ],
   providers: [],
