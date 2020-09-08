a44a01322da05dafebee5d5a8a9fd6029b691ebb
Theo Dias Mitchell <theomitchell@gmail.com>
2020-09-08 17:03:45 -0400

    User Login Validation and error messages

--- TDM_OnLineStore.Web/ClientApp/src/app/user/login/login.component.ts
+++ TDM_OnLineStore.Web/ClientApp/src/app/user/login/login.component.ts
@@ -10,7 +10,7 @@ import { User } from "../../model/user";
 
 export class LoginComponent {
   public user;
-  public authentifiedUser: boolean;
+  //public authentifiedUser: boolean;
 
   constructor() {
     this.user = new User();
